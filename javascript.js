c2f93493218c108c76039603617c4139ad5d1a71
SY-Training <92039698+SY-Training@users.noreply.github.com>
2021-11-10 13:32:32 +0000

    Add files via upload
    Almost complete JS code. Only need to add 5 round play.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,35 @@
 const rock = document.querySelector('#rock');
-rock.onclick = () => console.log("Clicked ROCK");
-// set to play game
 
 const paper = document.querySelector('#paper');
-paper.onclick = () => console.log("Clicked PAPER");
 
 const scissors = document.querySelector('#scissors');
-scissors.onclick = () => console.log("Clicked SCISSORS");
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>
+{
+    button.addEventListener('click', () => 
+    {
+        playRound(button.id)
+        
+    });
+});
+
+let playerScore;
+let compScore;
+
+const pScore = document.querySelector('.playerScore');
+pScore.textContent = playerScore;
+
+const aiScore = document.querySelector('.aiScore');
+aiScore.textContent = compScore;
+
+const result = document.querySelector('.result');
+
 
 
 
 function playerChoice(select) {
 
-    //if button was scissors, select scissors
-    //else if paper
-    
-    // Error if player chooses wrong. 
     if (select == "rock" || select == "paper" || select == "scissors"){
             console.log("You chose: " + select);                        
     }
@@ -26,6 +40,80 @@ function playerChoice(select) {
     return select;               
 }
 
+// AI randomly selects choice.
+function computerChoice(AIChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    
+    AIChoice = choices[Math.floor(Math.random() * choices.length)]
+
+    return AIChoice;
+}
+
+function playRound(player) {
+
+    console.log(player)
+    let comp = computerChoice();
+
+    const p = "paper";
+    const r = "rock";
+    const s = "scissors"
+
+    playerChoice(player);
+
+    //compare player and comp
+
+    if (player === p && comp === r){
+        console.log("You win! You chose " + player + ", which beats " + comp);
+        playerScore++;
+    }
+
+    else if (player === s && comp === p){
+        console.log("You win! You chose " + player + ", which beats " + comp);
+        playerScore++;
+    }
+
+    else if (player === r && comp === s){
+        console.log("You win! You chose " + player + ", which beats " + comp);
+        playerScore++;
+    }
+
+    else if (player === comp){
+        console.log("It's a tie! You both chose " + player);
+    }
+
+    else {
+        console.log("You lose! You chose " + player + ", which loses to " + comp);
+        compScore++;
+    }
+
+} 
+
+function game() {
+
+    let round = 0;
+        
+    while (round < 5){
+        console.log("Round: " + round);
+        playRound();
+        console.log(playerScore + "    " + compScore);
+        round++;
+    }
+             
+
+    if (playerScore > compScore) {
+        console.log("You win! Your score was " + playerScore + ", which beats " + compScore);
+    }
+
+    if (playerScore < compScore) {
+        console.log("You lose! Your score was " + playerScore + ", which loses to " + compScore);
+    }
+    
+    if (playerScore === compScore){
+        console.log("It's a tie! You both scored " + playerScore);
+    }
+
+
+}
 
 function ChangeColour()
 {
